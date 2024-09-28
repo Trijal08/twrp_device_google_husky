4fa9cd0cedefbab47e961b3af40259784c5212f1
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-09-27 22:41:40 -0400

    Add support for SHRP

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
+## OrangeFox variables:
 # Some about us
 export FOX_VERSION="R11.1"
 export OF_MAINTAINER="GamerBoy1234"
@@ -88,3 +89,59 @@ export FOX_BASH_TO_SYSTEM_BIN=1
 export FOX_ENABLE_APP_MANAGER=1
 export FOX_VARIANT="default"
 export OF_USE_GREEN_LED=1
+
+## SkyHawk variables:
+# Maintainer flags
+export SHRP_MAINTAINER="GamerBoy1234"
+
+# Build environment stuff
+export SHRP_DEVICE_CODE=husky
+export ALLOW_MISSING_DEPENDENCIES=true
+export SHRP_PATH=device/google/$SHRP_DEVICE_CODE
+
+# Recovery Type (for "About" section only)
+# Default (if not set): N/A
+export SHRP_REC_TYPE=Normal
+
+# Device Type (for "About" section only)
+# Default (if not set): N/A
+export SHRP_DEVICE_TYPE=A/B
+
+# Your device's recovery path, dont use blindly
+# Default (if not set): N/A
+export SHRP_REC=/dev/block/platform/13200000.ufs/by-name/vendor_boot
+
+# Use this flag only if SHRP_REC is set
+# Default (if not set): N/A
+export SHRP_HAS_RECOVERY_PARTITION=true
+
+# Use this flag only if your device is A/B or Virtual A/B.
+# Default (if not set): N/A
+export SHRP_AB=true
+
+# Emergency DownLoad mode (0 = no EDL mode, 1 = EDL mode available)
+# Default (if not set): 0
+export SHRP_EDL_MODE=0
+
+# External SDcard path
+# Default (if not set): /
+export SHRP_EXTERNAL=/external_sd
+
+# USB OTG path
+# Default (if not set): /
+export SHRP_OTG=/usb_otg
+
+# SHRP padding flag (for rounded corner devices only)
+# You have to change these values according to your device's roundness.
+export SHRP_STATUSBAR_RIGHT_PADDING=48
+# Default (for LEFT): 20
+export SHRP_STATUSBAR_LEFT_PADDING=48
+
+# For notch devices
+# Default (if not set): N/A
+export SHRP_NOTCH=false
+
+# SHRP Dark mode, use this flag to have dark theme set by default
+# Default (if not set) is not using DARK mode
+export SHRP_DARK=true
+
