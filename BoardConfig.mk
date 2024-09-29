4a9687b944a6f955c3ac18d582f78862e9854647
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2024-09-29 21:57:29 +0200

    Readjust padding and disable vibration for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -230,8 +230,8 @@ PLATFORM_VERSION := 99.87.36
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
 # Vibrator
-TW_SUPPORT_INPUT_AIDL_HAPTICS := true
-TW_SKIP_ADDITIONAL_FSTAB := true
+#TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+#TW_SKIP_ADDITIONAL_FSTAB := true
 
 # sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
@@ -263,7 +263,7 @@ TW_INCLUDE_LIBRESETPROP_SOURCE := true
 TW_EXCLUDE_APEX := true
 #TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_USE_CRYPTO := true
-TW_Y_OFFSET := 40
-TW_H_OFFSET := -40
+TW_Y_OFFSET := 28
+TW_H_OFFSET := -28
 TW_OVERRIDE_SYSTEM_PROPS := \ 
 "ro.bootimage.build.date.utc=ro.build.date.utc;ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.name=ro.product.system.name"
