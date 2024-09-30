e138116ae85ed92df8303ffc2d2a7c9530ddc3b3
Trijal Saha <trijalsaha2012@gmail.com>
2024-09-30 19:53:30 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -100,8 +100,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    android.hardware.fastboot@1.0-impl-mock.recovery \
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock.recovery \
     android.hardware.fastboot@1.1-impl.pixel \
     fastbootd
 
