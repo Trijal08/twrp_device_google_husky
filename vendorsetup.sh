a6333997a5cb0ec6409b8fa46fa2dc7bbdfb01ef
Trijal Saha <trijalsaha2012@gmail.com>
2024-10-19 17:59:17 -0400

    Revert "Old stuff"
    This reverts commit 755014b9631a4ad748dc73ec9c9a9bbe4895f207.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 ## OrangeFox variables:
+# General information
 export FOX_VERSION="R11.1"
+export OF_MAINTAINER="GamerBoy1234294"
 export OF_MAINTAINER_AVATAR="device/google/husky/maintainer.png"
 
 # Build environment stuffs
 export FOX_BUILD_DEVICE="Pixel8Pro"
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export TARGET_DEVICE_ALT="Pixel8Pro, GooglePixel8Pro, husky, Husky, shusky, Shusky"
+export FOX_TARGET_DEVICES="Pixel8Pro, GooglePixel8Pro, husky, Husky, shusky, Shusky"
 
 # Use magisk boot for patching
 export OF_USE_MAGISKBOOT=1
@@ -16,6 +20,7 @@ export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 # Vanilla build
 export FOX_VANILLA_BUILD=1
 
+# We have A/B partition scheme
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
 
@@ -48,17 +53,17 @@ export FOX_BUILD_BASH=1
 export OF_RUN_POST_FORMAT_PROCESS=1
 
 # Disable decryption
+export OF_SKIP_FBE_DECRYPTION=1
 
 # Use /data/recovery/Fox/ for Storage
 export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
 
-# For some reason this is dumb and necessary
-export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
-
 export OF_QUICK_BACKUP_LIST="/boot;/init_boot;/data;"
 
 # Magisk
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v28.0.zip
 
+# Dont install AromaFM
 export FOX_DELETE_AROMAFM=1
 
 # Add some extras
@@ -68,8 +73,8 @@ export FOX_USE_SED_BINARY=1
 export FOX_USE_XZ_UTILS=1
 export FOX_USE_LZ4_BINARY=1
 export FOX_USE_ZSTD_BINARY=1
-export FOX_ASH_IS_BASH=1
 export FOX_REPLACE_BUSYBOX_PS=1
+export FOX_USE_BUSYBOX_BINARY=1
 export FOX_USE_BASH_SHELL=1
 export OF_USE_LZ4_COMPRESSION=1 
 export FOX_USE_NANO_EDITOR=0
@@ -77,7 +82,6 @@ export OF_DONT_KEEP_LOG_HISTORY=1
 export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=0
 export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
 export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-export OF_ENABLE_FS_COMPRESSION=1
 export FOX_REPLACE_TOOLBOX_GETPROP=1
 export FOX_BASH_TO_SYSTEM_BIN=1
 export FOX_ENABLE_APP_MANAGER=1
@@ -107,7 +111,7 @@ export SHRP_REC=/dev/block/platform/13200000.ufs/by-name/vendor_boot
 
 # Use this flag only if SHRP_REC is set
 # Default (if not set): N/A
-export SHRP_HAS_RECOVERY_PARTITION=true
+export SHRP_HAS_RECOVERY_PARTITION=false
 
 # Use this flag only if your device is A/B or Virtual A/B.
 # Default (if not set): N/A
@@ -138,4 +142,3 @@ export SHRP_NOTCH=false
 # SHRP Dark mode, use this flag to have dark theme set by default
 # Default (if not set) is not using DARK mode
 export SHRP_DARK=true
-
